7be2298f564f205bf2356b816b421645f2d8ae15
cmakario <cmakario@student.42.fr>
2025-04-19 13:57:35 +0200

    implement seriualization

--- ex01/src/Serializer.cpp
+++ ex01/src/Serializer.cpp
@@ -6,7 +6,23 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 13:53:52 by cmakario          #+#    #+#             */
-/*   Updated: 2025/04/19 13:53:53 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/04/19 13:56:52 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+
+
+Serializer::Serializer() {}
+Serializer::Serializer(const Serializer &src) {(void)src;}
+Serializer &Serializer::operator=(const Serializer &src) {(void)src; return *this;}
+Serializer::~Serializer() {}
+
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+Data *Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+}
