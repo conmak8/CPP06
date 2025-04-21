e320121f5ae890ddd0772e292b580a0c58dee33e
cmakario <cmakario@student.42.fr>
2025-04-21 11:31:18 +0200

    create base hpp of base,a,b,c

---
+++ ex02/include/Identity.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Identity.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 11:29:38 by cmakario          #+#    #+#             */
+/*   Updated: 2025/04/21 11:30:35 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IDENTITY_HPP
+#define IDENTITY_HPP
+
+#include "Base.hpp"
+
+Base* generate();
+void identify(Base *p);
+void identify(Base &p);
+
+#endif // !IDENTITY_HPP
